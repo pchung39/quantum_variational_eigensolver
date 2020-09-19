1b14827cf6007a20b3e488aaaeddc68c14e809cb
Paul Chung <paul.chung@jpl.nasa.gov>
2020-09-19 16:30:51 -0700

    slowly testing out two qubit vqe circuit

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from qiskit.circuit.library.standard_gates import U2Gate
 from qiskit.aqua.operators import WeightedPauliOperator
 from qiskit.aqua.algorithms import NumPyEigensolver
 import matplotlib.pyplot as plt
+from qiskit.tools.visualization import plot_histogram
 
 '''
 Classical Model
@@ -55,28 +56,30 @@ def quantum_state_preparation(circuit):
     # can use np format: np.array([np.pi, np.pi])
     circuit.rx(0,0)
     circuit.rx(0,1)
-    circuit.cx(0,1)
-    circuit.cx(1,0)
-    circuit.h(0)
-    circuit.h(1)
-
-    circuit.barrier()
-
-    '''
-    Hamiltonian decomposition rotations (rotations are regulated by the decomposition)
-    '''
-    circuit.rx(0,0)
-    circuit.rx(0,0)
-
-    circuit.ry(0,1)
-    circuit.ry(0,1)
     
+    # circuit.cx(1,0)
+    circuit.h(0)
+    circuit.cx(0,1)
+    circuit.h(1)
+    circuit.cx(1,0)
+
     circuit.barrier()
+
+    # '''
+    # Hamiltonian decomposition rotations (rotations are regulated by the decomposition)
+    # '''
+    # circuit.rx(0,0)
+    # circuit.rx(0,0)
+
+    # circuit.ry(0,1)
+    # circuit.ry(0,1)
+    
+    # circuit.barrier()
     return circuit
 
 
 
-def vqe_circuit(parameters, measure):
+def vqe_circuit(measure):
     """
     Creates a device ansatz circuit for optimization.
     :param parameters_array: list of parameters for constructing ansatz state that should be optimized.
@@ -84,33 +87,40 @@ def vqe_circuit(parameters, measure):
     :return: quantum circuit.
     """
     q = QuantumRegister(2)
-    c = ClassicalRegister(1)
+    c = ClassicalRegister(2)
     circuit = QuantumCircuit(q, c)
 
     # quantum state preparation
-    circuit = quantum_state_preparation(circuit, parameters)
+    circuit = quantum_state_preparation(circuit)
 
     # measurement
+    # this is where you rotate either on the x or y axis and then measure 
     if measure == 'Z':
         circuit.measure(q[0], c[0])
+        circuit.measure(q[1], c[1])
     elif measure == 'X':
         circuit.u2(0, np.pi, q[0])
+        circuit.u2(0, np.pi, q[1])
         circuit.measure(q[0], c[0])
+        circuit.measure(q[1], c[1])
     elif measure == 'Y':
         circuit.u2(0, np.pi/2, q[0])
+        circuit.u2(0, np.pi/2, q[1])
         circuit.measure(q[0], c[0])
+        circuit.measure(q[1], c[1])
     else:
         raise ValueError('Not valid input for measurement: input should be "X" or "Y" or "Z"')
 
     return circuit
 
-q = QuantumRegister(2)
-c = ClassicalRegister(1)
-circuit = QuantumCircuit(q, c)
+# q = QuantumRegister(2)
+# c = ClassicalRegister(1)
+# circuit = QuantumCircuit(q, c)
 
-test_circuit = quantum_state_preparation(circuit)
-test_circuit.draw(output="mpl")
-plt.show()
+# test_circuit = quantum_state_preparation(circuit)
+# test_circuit = vqe_circuit("X")
+# test_circuit.draw(output="mpl")
+# plt.show()
 
 
 def quantum_module(parameters, measure):
@@ -126,20 +136,35 @@ def quantum_module(parameters, measure):
     else:
         raise ValueError('Not valid input for measurement: input should be "I" or "X" or "Z" or "Y"')
     
-    shots = 8192
+    # TODO: tweak this to see if values change?
+    shots = 1000
     backend = BasicAer.get_backend('qasm_simulator')
     job = execute(circuit, backend, shots=shots)
     result = job.result()
     counts = result.get_counts()
+    plot_histogram(counts)
     
     # expectation value estimation from counts
-    expectation_value = 0
-    for measure_result in counts:
-        sign = +1
-        if measure_result == '1':
-            sign = -1
-        expectation_value += sign * counts[measure_result] / shots
-        
+    # expectation_value = 0
+    # for measure_result in counts:
+    #     sign = +1
+    #     if measure_result == '1':
+    #         sign = -1
+    #     expectation_value += sign * counts[measure_result] / shots
+
+
+    if measure == 'ZZ':
+        expected_value = (counts['00'] - counts['01'] - counts['10'] + counts['11']) / num_shots
+    elif measure == 'XX':
+        expected_value = (counts['00'] + counts['01'] + counts['10'] + counts['11']) / num_shots
+    elif measure == "YY":
+        expected_value = (counts['00'] + counts['01'] + counts['10'] + counts['11']) / num_shots
+    elif measure == "II":
+        expected_value = (counts['00'] + counts['01'] + counts['10'] + counts['11']) / num_shots
+    else:
+        raise ValueError('Not valid input for measurement: input should be "I" or "X" or "Z" or "Y"')    
+
+    print("Expectation value for paul matrix ({}): {}".format(measure,m expected_value))
     return expectation_value
 
 def pauli_operator_to_dict(pauli_operator):
@@ -161,6 +186,20 @@ def pauli_operator_to_dict(pauli_operator):
     return paulis_dict
 
 
+def hamiltonian_operator(a, b, c, d):
+    """
+    Creates a*I + b*Z + c*X + d*Y pauli sum 
+    that will be our Hamiltonian operator.
+    
+    """
+    pauli_dict = {
+        'paulis': [{"coeff": {"imag": 0.0, "real": a}, "label": "II"},
+                   {"coeff": {"imag": 0.0, "real": b}, "label": "ZZ"},
+                   {"coeff": {"imag": 0.0, "real": c}, "label": "XX"},
+                   {"coeff": {"imag": 0.0, "real": d}, "label": "YY"}
+                   ]
+    }
+    return WeightedPauliOperator.from_dict(pauli_dict)
 
 def vqe(parameters):
 
@@ -173,10 +212,18 @@ def vqe(parameters):
     
     # summing the measurement results
     classical_adder = quantum_module_I + quantum_module_Z + quantum_module_X + quantum_module_Y
-    
+    print("Added values: ", classical_adder)
     return classical_adder
 
-# parameters_array = np.array([np.pi, np.pi])
+
+a, b, c, d = (0.5, 0.5, -0.5, -0.5)
+
+H = hamiltonian_operator(a, b, c, d)
+print("H: ", H.print_details())
+parameters_array = np.array([np.pi, np.pi])
+test_vqe = vqe(parameters_array)
+
+
 # tol = 1e-3 # tolerance for optimization precision.
 
 # vqe_result = minimize(vqe, parameters_array, method="Powell", tol=tol)
